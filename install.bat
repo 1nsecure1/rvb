2377e2bad6babb8688f234ef9052f7ba1f3d6040
1nsecure1 <54017103+1nsecure1@users.noreply.github.com>
2023-04-21 13:35:37 -0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,48 +1,33 @@
-@echo off
+# Install curl
+powershell.exe -Command "Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))"
+choco install curl -y
 
-set "TARGET_DIR=%USERPROFILE%\Downloads\"
+# Install Git for Windows
+curl -LO https://github.com/git-for-windows/git/releases/download/v2.31.1.windows.1/Git-2.31.1.1-64-bit.exe
+7z x Git-2.31.1.1-64-bit.exe -oGitSetup
+./GitSetup/git-bash.exe ./GitSetup/setup-x86_64.exe /SP- /VERYSILENT /NORESTART
 
-echo Installing 7-Zip...
-curl -Lo 7z.exe https://www.7-zip.org/a/7z2103-x64.exe
-start /wait 7z.exe /S
+# Install Python 3.10
+curl -LO https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
+./python-3.10.0-amd64.exe /quiet InstallAllUsers=1 PrependPath=1
 
-echo Installing Git...
-curl -Lo git.zip https://github.com/git-for-windows/git/releases/download/v2.33.1.windows.1/MinGit-2.33.1-64-bit.zip
-7z x git.zip -o"%USERPROFILE%\Downloads"
-set "PATH=%PATH%;%USERPROFILE%\Downloads\cmd;"
+# Install 7-Zip
+curl -LO https://www.7-zip.org/a/7z2104-x64.exe
+7z x 7z2104-x64.exe -o7zSetup
+./7zSetup/7z1900-x64.exe /S
 
-echo Installing Python...
-curl -Lo python.exe https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
-start /wait python.exe /quiet InstallAllUsers=1 PrependPath=1 TargetDir=C:\Python310
-set "PATH=%PATH%;C:\Python310;C:\Python310\Scripts;"
+# Install Canarytokens
+git clone https://github.com/thinkst/canarytokens.git
 
-echo Downloading Cowrie...
-git clone https://github.com/adhdproject/cowrie.git "%TARGET_DIR%cowrie"
+# Install Cowrie
+git clone https://github.com/adhdproject/cowrie.git
 
-echo Downloading Impostor...
-git clone https://github.com/superswan/impostor.git "%TARGET_DIR%impostor"
+# Install Impostor
+git clone https://github.com/superswan/impostor.git
 
-echo Downloading Canarytokens...
-git clone https://github.com/thinkst/canarytokens.git "%TARGET_DIR%canarytokens"
-
-echo Downloading Portspoof...
-git clone https://github.com/drk1wi/portspoof.git "%TARGET_DIR%portspoof"
-
-echo Installing Cowrie...
-cd "%TARGET_DIR%cowrie"
-python setup.py install
-
-echo Installing Impostor...
-cd "%TARGET_DIR%impostor"
-python setup.py install
-
-echo Installing Canarytokens...
-cd "%TARGET_DIR%canarytokens"
-python setup.py install
-
-echo Installing Portspoof...
-cd "%TARGET_DIR%portspoof"
+# Install Portspoof
+git clone https://github.com/drk1wi/portspoof.git
+cd portspoof
+./configure
 make
-make install
-
-echo Done.
+sudo make install
