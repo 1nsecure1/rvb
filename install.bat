5b143c1a6fb3da89bb83d4ce6c92c4809ea79773
1nsecure1 <54017103+1nsecure1@users.noreply.github.com>
2023-04-21 13:29:40 -0700

    Update install.bat

--- install.bat
+++ install.bat
@@ -1,79 +1,48 @@
-#!/bin/bash
+@echo off
 
-# Check if the script is being run on a Linux machine
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Navigate to the directory where you want to download the repositories
-    cd ~
-    
-    # Install git if it is not already installed
-    if ! [ -x "$(command -v git)" ]; then
-        sudo apt-get update
-        sudo apt-get install git -y
-    fi
-    
-    # Clone the impostor repository
-    git clone https://github.com/superswan/impostor.git
-    cd impostor
-    
-    # Install the necessary dependencies for impostor
-    sudo apt-get install build-essential libsdl2-dev -y
-    
-    # Build the impostor executable
-    make
-    
-    # Set the necessary permissions to run the impostor executable
-    chmod +x impostor
-    
-    # Navigate back to the home directory
-    cd ~
-    
-    # Clone the cowrie repository
-    git clone https://github.com/adhdproject/cowrie.git
-    
-    # Clone the canarytokens repository
-    git clone https://github.com/thinkst/canarytokens.git
-    
-    # Clone the portspoof repository
-    git clone https://github.com/drk1wi/portspoof.git
-    
-# Check if the script is being run on a Windows machine
-elif [[ "$OSTYPE" == "msys"* ]]; then
-    # Navigate to the directory where you want to download the repositories
-    cd ~
-    
-    # Install Git for Windows if it is not already installed
-    if ! [ -x "$(command -v git)" ]; then
-        curl -o Git-2.32.0.2-64-bit.exe https://github.com/git-for-windows/git/releases/download/v2.32.0.windows.2/Git-2.32.0.2-64-bit.exe
-        ./Git-2.32.0.2-64-bit.exe /SP- /VERYSILENT /SUPPRESSMSGBOXES /CLOSEAPPLICATIONS /NORESTART
-        rm Git-2.32.0.2-64-bit.exe
-    fi
-    
-    # Clone the impostor repository
-    git clone https://github.com/superswan/impostor.git
-    cd impostor
-    
-    # Install the necessary dependencies for impostor using the Git Bash terminal
-    pacman -S mingw-w64-x86_64-make mingw-w64-x86_64-SDL2
-    
-    # Build the impostor executable
-    mingw32-make
-    
-    # Set the necessary permissions to run the impostor executable
-    chmod +x impostor.exe
-    
-    # Navigate back to the home directory
-    cd ~
-    
-    # Clone the cowrie repository
-    git clone https://github.com/adhdproject/cowrie.git
-    
-    # Clone the canarytokens repository
-    git clone https://github.com/thinkst/canarytokens.git
-    
-    # Clone the portspoof repository
-    git clone https://github.com/drk1wi/portspoof.git
-    
-else
-    echo "Unsupported operating system: $OSTYPE"
-    exit 1
-fi
+set "TARGET_DIR=%USERPROFILE%\Downloads\"
+
+echo Installing 7-Zip...
+curl -Lo 7z.exe https://www.7-zip.org/a/7z2103-x64.exe
+start /wait 7z.exe /S
+
+echo Installing Git...
+curl -Lo git.zip https://github.com/git-for-windows/git/releases/download/v2.33.1.windows.1/MinGit-2.33.1-64-bit.zip
+7z x git.zip -o"%USERPROFILE%\Downloads"
+set "PATH=%PATH%;%USERPROFILE%\Downloads\cmd;"
+
+echo Installing Python...
+curl -Lo python.exe https://www.python.org/ftp/python/3.10.0/python-3.10.0-amd64.exe
+start /wait python.exe /quiet InstallAllUsers=1 PrependPath=1 TargetDir=C:\Python310
+set "PATH=%PATH%;C:\Python310;C:\Python310\Scripts;"
+
+echo Downloading Cowrie...
+git clone https://github.com/adhdproject/cowrie.git "%TARGET_DIR%cowrie"
+
+echo Downloading Impostor...
+git clone https://github.com/superswan/impostor.git "%TARGET_DIR%impostor"
+
+echo Downloading Canarytokens...
+git clone https://github.com/thinkst/canarytokens.git "%TARGET_DIR%canarytokens"
+
+echo Downloading Portspoof...
+git clone https://github.com/drk1wi/portspoof.git "%TARGET_DIR%portspoof"
+
+echo Installing Cowrie...
+cd "%TARGET_DIR%cowrie"
+python setup.py install
+
+echo Installing Impostor...
+cd "%TARGET_DIR%impostor"
+python setup.py install
+
+echo Installing Canarytokens...
+cd "%TARGET_DIR%canarytokens"
+python setup.py install
+
+echo Installing Portspoof...
+cd "%TARGET_DIR%portspoof"
+make
+make install
+
+echo Done.
